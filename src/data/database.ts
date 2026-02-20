b99220063bd5d64094a01a24b253e944efc7b211
Minh Phan <minhsg.phan@gmail.com>
2026-02-20 17:46:08 -0600

    format according to prettier

--- src/data/database.ts
+++ src/data/database.ts
@@ -1,18 +1,18 @@
-import sqlite3 from 'sqlite3'
-import { Database, open} from 'sqlite'
-import {card} from "../types/types";
+import sqlite3 from "sqlite3";
+import { Database, open } from "sqlite";
+import { card } from "../types/types";
 
 export class AppDB {
-    private static instance: AppDB
-    private db: Database
+    private static instance: AppDB;
+    private db: Database;
 
     private constructor() {
         (async () => {
             // open the database
             this.db = await open({
-                filename: 'src/data/cards.db',
-                driver: sqlite3.Database
-            })
+                filename: "src/data/cards.db",
+                driver: sqlite3.Database,
+            });
 
             // create decks table
             await this.db.exec(`
@@ -24,7 +24,7 @@ export class AppDB {
                     created     TEXT NOT NULL DEFAULT CURRENT_DATE,
                     uses        INTEGER       DEFAULT 0 NOT NULL
                 );
-            `)
+            `);
 
             // create cards table
             await this.db.exec(`
@@ -38,8 +38,8 @@ export class AppDB {
 
                     UNIQUE (cardId, deckId)
                 );
-            `)
-        })()
+            `);
+        })();
     }
 
     public static getInstance(): AppDB {
@@ -49,21 +49,29 @@ export class AppDB {
         return AppDB.instance;
     }
 
-    public async getCardByCardId(cardId: string, deckId: string): Promise<card> {
-        let result : card
-        result = await this.db.get('SELECT * FROM cards WHERE cardId = :cardId AND deckId = :deckId', {
-            ':cardId': cardId,
-            ':deckId': deckId
-        })
-        return result
+    public async getCardByCardId(
+        cardId: string,
+        deckId: string,
+    ): Promise<card> {
+        let result: card;
+        result = await this.db.get(
+            "SELECT * FROM cards WHERE cardId = :cardId AND deckId = :deckId",
+            {
+                ":cardId": cardId,
+                ":deckId": deckId,
+            },
+        );
+        return result;
     }
 
     public async getCardsByDeckId(deckId: string): Promise<card[]> {
-        let result : card[]
-        result = await this.db.get('SELECT * FROM cards WHERE deckId = :deckId', {
-            ':deckId': deckId
-        })
-        return result
+        let result: card[];
+        result = await this.db.get(
+            "SELECT * FROM cards WHERE deckId = :deckId",
+            {
+                ":deckId": deckId,
+            },
+        );
+        return result;
     }
-
 }
