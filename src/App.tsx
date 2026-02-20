c2e9739b86b5f85e81065c7c1afb85bc6d0ba00f
Atishaya Maharjan <71453175+Atishaya7777@users.noreply.github.com>
2026-02-20 14:08:46 -0600

    feat: Added react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
     return (
-        <div>
-            <h1>Hello World!</h1>
-        </div>
+        <Routes>
+            <Route path="/" element={<div>Home</div>} />
+            <Route path="/flashcard" element={<div>Flashcard</div>} />
+        </Routes>
     )
 }
 
