be884bc657b1bb913e085aed1e5b6916f56a2216
Minh Phan <minhsg.phan@gmail.com>
2026-02-20 15:09:08 -0600

    implement singleton db object

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,20 +1,26 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
+import {Database} from "sqlite3"
 
-class Database {
-    private static instance: Database;
-
-    private db: sqlite3.Database;
+class AppDB {
+    private static instance: AppDB;
+    private db: Database
 
     private constructor() {
-        // Private constructor to prevent direct instantiation
+        (async () => {
+            // open the database
+            const db = await open({
+                filename: '/cards.db',
+                driver: sqlite3.Database
+            })
+        })()
     }
 
-}
-async function initDatabase() {
-    // open the database
-    const db = await open({
-        filename: 'cards.db',
-        driver: sqlite3.Database
-    })
+    public getInstance() : AppDB {
+        if (!AppDB.instance) {
+            AppDB.instance = new AppDB();
+        }
+        return AppDB.instance;
+    }
+
 }
