2b1c11e083db345e9483bfc62a3e5bcc493a2bf4
Keira <keirapasveer@gmail.com>
2026-02-20 15:51:34 -0600

    updated create tables script: cardId and deckId are now created by parsing script

--- src/database/create_tables.sql
+++ src/database/create_tables.sql
@@ -2,7 +2,7 @@
 -- Must be done BEFORE cards table is created
 
 CREATE TABLE IF NOT EXISTS decks (
-    deckId INTEGER PRIMARY KEY AUTOINCREMENT,
+    deckId TEXT PRIMARY KEY,
     filepath TEXT NOT NULL,
     lastUpdated TEXT NOT NULL DEFAULT CURRENT_DATE,
     created TEXT NOT NULL DEFAULT CURRENT_DATE,
@@ -12,11 +12,11 @@ CREATE TABLE IF NOT EXISTS decks (
 -- Creates the cards table
 
 CREATE TABLE IF NOT EXISTS cards (
-    cardId INTEGER PRIMARY KEY AUTOINCREMENT,
+    cardId TEXT PRIMARY KEY, 
     deckId INTEGER NOT NULL,
     question TEXT NOT NULL,
     answer TEXT NOT NULL,
     laters INTEGER DEFAULT 0 NOT NULL,
     
-    UNIQUE (cardId, deckId) 
+    PRIMARY KEY (cardId, deckId) 
 );
