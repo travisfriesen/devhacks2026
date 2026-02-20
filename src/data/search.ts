ace4508e857e6cbc10954f5e0c2b38a1394d3073
travisfriesen <github@travisfriesen.ca>
2026-02-20 16:07:07 -0600

    feat: search functions awaiting querying the db

---
+++ src/data/search.ts
@@ -0,0 +1,31 @@
+import { card, deck } from "../types/types";
+import {getDecks} from "./deck";
+import {getAllCards} from "./cards";
+
+/**
+ * Searches by keyword and returns all decks that contain the keyword,
+ * all cards (questions or answers) that contains the keywords.
+ * Returns empty arrays if nothing is found.
+ * @param keyword
+ */
+function searchByKeyword(keyword: string): [decks: deck[], cards: card[]] {
+    return [searchDecks(keyword), searchCards(keyword)];
+}
+
+/**
+ * Searches all decks in the database by keyword and returns all decks that contain the keyword.
+ * Returns an empty array if nothing is found.
+ * @param keyword
+ */
+function searchDecks(keyword: string): deck[] {
+    return null
+}
+
+/**
+ * Searches all cards in the database by keyword and returns all cards that contain the keyword.
+ * Returns an empty array if nothing is found.
+ * @param keyword
+ */
+function searchCards(keyword:string): card[] {
+    return null
+}
