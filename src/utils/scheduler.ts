0b3ac896baf7e681383ed76e1046da7c36f2166f
Keira <keirapasveer@gmail.com>
2026-02-21 10:22:10 -0600

    tomorrow function

--- src/utils/scheduler.ts
+++ src/utils/scheduler.ts
@@ -1,4 +1,5 @@
 import { ICard } from "@/types/types";
+import { updateCardLaters, updateCardDueDate } from "../data/cards.ts"
 
 export function fibonacci(n: number): number {
     // Leaving this to Keira. Hopefully it's not a O(2^n) algorithm
@@ -45,12 +46,42 @@ export function scheduleCard(
     switch (rating) {
         case 1: // again
             // We should push to top of the queue to give it again instantly
+            // Done in UI logic
             break;
         case 2: // later-session
             // Insert somewhere inside of the queue (middle? random?)
+            // Done in UI logic
             break;
         case 3: // next-session
             // This is due tomorrow, so leave the queue but update the date in the card
+            const tomorrow = (): Date => {
+                let day = card.dueDate.getDay();
+                let month = card.dueDate.getMonth();
+                let year = card.dueDate.getFullYear();
+
+                const day31 = [1, 3, 5, 7, 8, 10, 12];
+                const day30 = [4, 6, 9, 11];
+                const day28 = [2];
+
+                if (day31.includes(month) && day == 31 
+                    || day30.includes(month) && day == 30 
+                    || day28.includes(month) && day == 28) {
+                    day = 1;
+                    
+                    if (month == 12) {
+                        year++;
+                        month = 1;
+                    }
+                    else {
+                        month++;
+                    }
+                }
+                else {
+                    day++;
+                }
+
+                return new Date(year, month, day);
+            } 
             break;
         case 4: // later
             // This depends on the fibonacci backoff date algorithm
