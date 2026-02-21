7bbe17473ccc371f2252362d09f0b57a6279e374
travisfriesen <github@travisfriesen.ca>
2026-02-21 15:35:23 -0600

    bump: node version and name

--- forge.config.ts
+++ forge.config.ts
@@ -9,7 +9,8 @@ import { FuseV1Options, FuseVersion } from "@electron/fuses";
 
 const config: ForgeConfig = {
     packagerConfig: {
-        asar: true,
+        name: "devhacks 2026",
+        executableName: "devhacks2026",
     },
     rebuildConfig: {},
     makers: [
