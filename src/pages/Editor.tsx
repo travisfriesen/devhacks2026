b05d97bf5e40bfac86e274dc71070f5854f18f47
travisfriesen <github@travisfriesen.ca>
2026-02-21 08:29:47 -0600

    feat: editor with box for question and answer's

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,24 +1,75 @@
-import {useState} from "react";
-import MDEditor from '@uiw/react-md-editor';
+import { useState } from "react";
+import { ICard } from "@/types/types"
+import MDEditor, {commands} from "@uiw/react-md-editor";
 
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 
-export default function Editor() {
-    const [content, setContent] = useState('');
+interface IEditorProps {
+    card: ICard;
+    setCard: (card: ICard) => void;
+}
+
+export default function Editor({ card, setCard }: IEditorProps) {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
 
     return (
         <div>
             <MDEditor
+                value={title}
+                onChange={setTitle}
+                preview={"preview"}
+                minHeight={20}
+                enableScroll={false}
+                commands={[]}
+                extraCommands={[]}
+                visibleDragbar={false}
+                // extraCommands={[commands.fullscreen]}
+                previewOptions={{
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                }}
+            />
+            <br />
+            <MDEditor
+                value={title}
+                onChange={setTitle}
+                preview={"edit"}
+                minHeight={20}
+                enableScroll={false}
+                extraCommands={[commands.fullscreen]}
+                previewOptions={{
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                }}
+            />
+            <br />
+            <MDEditor
+                value={content}
+                onChange={setContent}
+                preview={"preview"}
+                commands={[]}
+                extraCommands={[]}
+                visibleDragbar={false}
+                // extraCommands={[commands.fullscreen]}
+                previewOptions={{
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
+                }}
+            />
+            <br />
+            <MDEditor
                 value={content}
                 onChange={setContent}
                 preview={"edit"}
+                extraCommands={[commands.fullscreen]}
                 previewOptions={{
                     remarkPlugins: [remarkMath],
-                    rehypePlugins: [rehypeKatex]
+                    rehypePlugins: [rehypeKatex],
                 }}
-                />
-            <MDEditor.Markdown source={content} style={{ whiteSpace: 'pre-wrap' }} />
+            />
+
         </div>
-    )
+    );
 }
