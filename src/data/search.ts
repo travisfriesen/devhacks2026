3c5a50409d257dd73c9ebe85c021b5072e66c63e
travisfriesen <github@travisfriesen.ca>
2026-02-21 08:27:40 -0600

    feat: search queries and functions

--- src/data/search.ts
+++ src/data/search.ts
@@ -1,4 +1,8 @@
-import { card, deck } from "../types/types";
+import { ICard, IDeck } from "../types/types";
+import { getDatabase } from "./database";
+
+// @ts-expect-error aaaaHHHH
+let db: Database | undefined;
 
 /**
  * Searches by keyword and returns all decks that contain the keyword,
@@ -6,7 +10,10 @@ import { card, deck } from "../types/types";
  * Returns empty arrays if nothing is found.
  * @param keyword
  */
-function searchByKeyword(keyword: string): [decks: deck[], cards: card[]] {
+function searchByKeyword(keyword: string): [decks: IDeck[], cards: ICard[]] {
+    if (db === undefined) {
+        db = getDatabase();
+    }
     return [searchDecks(keyword), searchCards(keyword)];
 }
 
@@ -15,8 +22,16 @@ function searchByKeyword(keyword: string): [decks: deck[], cards: card[]] {
  * Returns an empty array if nothing is found.
  * @param keyword
  */
-function searchDecks(keyword: string): deck[] {
-    return null;
+function searchDecks(keyword: string): IDeck[] {
+    if (db === undefined) {
+        db = getDatabase();
+    }
+
+    const statement = db.prepare(
+        `SELECT * FROM decks WHERE name LIKE '%' + ? '%'`,
+    );
+
+    return statement.all(keyword);
 }
 
 /**
@@ -24,6 +39,14 @@ function searchDecks(keyword: string): deck[] {
  * Returns an empty array if nothing is found.
  * @param keyword
  */
-function searchCards(keyword: string): card[] {
-    return null;
+function searchCards(keyword: string): ICard[] {
+    if (db === undefined) {
+        db = getDatabase();
+    }
+
+    const statement = db.prepare(
+        `SELECT * FROM cards WHERE question LIKE '%' + ?  + '%' OR answer LIKE '%' + ? + '%'`,
+    );
+
+    return statement.all(keyword);
 }
