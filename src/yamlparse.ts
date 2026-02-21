002e30645de324441edbeeb4d2eada12f2a3fed4
tlmmy8246 <timmylieutenenat@gmail.com>
2026-02-20 20:40:07 -0600

    Update yaml for interface change

--- src/yamlparse.ts
+++ src/yamlparse.ts
@@ -1,18 +1,18 @@
 import fs from "fs"
 import YAML from "yaml"
-import {deck, card} from "./types/types" 
+import {IDeck, ICard} from "./types/types" 
 import path from "path"
 
-export function parseYaml(yamlPath: string) : deck{
+export function parseYaml(yamlPath: string) : IDeck{
     const myData = YAML.parse(fs.readFileSync(yamlPath, 'utf-8'))    
     
-    let cardLists: Array<card> = [];
+    let cardLists: Array<ICard> = [];
 
     let randomUUID: string = crypto.randomUUID(); 
 
     for (var i = 0; i < myData['cards'].length; i++) {
         let curr = myData['cards'][i];
-        let card: card = {
+        let card: ICard = {
             deckId: curr['deckId'] || randomUUID,
             cardId: curr['cardId'] || i+1,
             question: curr['question'],
@@ -23,12 +23,13 @@ export function parseYaml(yamlPath: string) : deck{
         cardLists.push(card)
     }
 
-    let newDeck: deck = {
+    let newDeck: IDeck = {
         deckId: myData['deckId'] || randomUUID,
         deckName: myData['name'] || myData['deckName'], // Fallback if user provides name or deckName
         filepath: myData['filepath'] || path.resolve(yamlPath),
         lastUpdated: myData['lastUpdated'],
         created: myData['created'],
+        lastUtilized: myData['lastUtilized'],
         uses: myData['uses'],
         streak: myData['streak'],
         cards: cardLists,
@@ -37,7 +38,7 @@ export function parseYaml(yamlPath: string) : deck{
     return newDeck
 }
 
-export function dumpDeck(deckInfo:deck, path:string) {
+export function dumpDeck(deckInfo:IDeck, path:string) {
     let doc = YAML.stringify(deckInfo)
     fs.writeFileSync(path+deckInfo.deckName+".yaml", doc, 'utf-8')
 }
