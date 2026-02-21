db3997d1636519cb186c4c4299d1ec2604f0d9ae
Minh Phan <minhsg.phan@gmail.com>
2026-02-20 20:16:00 -0600

    implement cards CRUD

--- src/data/database.ts
+++ src/data/database.ts
@@ -19,25 +19,29 @@ export class AppDB {
 
             // create tables
             await db.exec(`
-                CREATE TABLE IF NOT EXISTS decks
-                (
-                    deckId      INTEGER PRIMARY KEY AUTOINCREMENT,
-                    filepath    TEXT NOT NULL,
-                    lastUpdated TEXT NOT NULL DEFAULT CURRENT_DATE,
-                    created     TEXT NOT NULL DEFAULT CURRENT_DATE,
-                    uses        INTEGER       DEFAULT 0 NOT NULL
+                CREATE TABLE IF NOT EXISTS decks (
+                     deckId TEXT PRIMARY KEY,
+                     name TEXT NOT NULL,
+                     filepath TEXT NOT NULL,
+                     lastUpdated TEXT NOT NULL DEFAULT CURRENT_DATE,
+                     lastUtilized TEXT NOT NULL DEFAULT CURRENT_DATE,
+                     created TEXT NOT NULL DEFAULT CURRENT_DATE,
+                     uses INTEGER DEFAULT 0 NOT NULL,
+                     streak INTEGER DEFAULT 0 NOT NULL
                 );
             `);
 
             await db.exec(`
                 CREATE TABLE IF NOT EXISTS cards
                 (
-                    cardId   INTEGER PRIMARY KEY AUTOINCREMENT,
-                    deckId   INTEGER           NOT NULL,
-                    question TEXT              NOT NULL,
-                    answer   TEXT              NOT NULL,
-                    laters   INTEGER DEFAULT 0 NOT NULL,
-                    UNIQUE (cardId, deckId)
+                    cardId TEXT UNIQUE,
+                    deckId INTEGER NOT NULL,
+                    question TEXT NOT NULL,
+                    answer TEXT NOT NULL,
+                    laters INTEGER DEFAULT 0 NOT NULL,
+                    dueDate TEXT NOT NULL DEFAULT CURRENT_DATE,
+
+                    PRIMARY KEY (cardId, deckId)
                 );
             `);
 
@@ -51,32 +55,23 @@ export class AppDB {
         cardId: string,
         deckId: string,
     ): Promise<card> {
-        let result: card;
-        result = await this.db.get(
+        return await this.db.get(
             "SELECT * FROM cards WHERE cardId = :cardId AND deckId = :deckId",
             {
                 ":cardId": cardId,
                 ":deckId": deckId,
             },
         );
-        return result;
     }
 
     public async retrieveCardsByDeckId(deckId: string): Promise<card[]> {
-        let result: card[];
-        result = await this.db.get(
-            "SELECT * FROM cards WHERE deckId = :deckId",
-            {
-                ":deckId": deckId,
-            },
-        );
-        return result;
+        return await this.db.get("SELECT * FROM cards WHERE deckId = :deckId", {
+            ":deckId": deckId,
+        });
     }
 
     public async retrieveAllCards(): Promise<card[]> {
-        let result: card[];
-        result = await this.db.get("SELECT * FROM cards");
-        return result;
+        return await this.db.get("SELECT * FROM cards");
     }
 
     public async createCard(card: card, deckId: string): Promise<boolean> {
@@ -92,6 +87,97 @@ export class AppDB {
             );
         } catch (error) {
             console.error("Error creating card:", error);
+            return false;
+        }
+    }
+
+    public async deleteCard(cardId: string, deckId: string): Promise<boolean> {
+        try {
+            await this.db.run(
+                "DELETE FROM cards WHERE cardId = :cardId AND deckId = :deckId",
+                {
+                    ":cardId": cardId,
+                    ":deckId": deckId,
+                },
+            );
+        } catch (error) {
+            console.error("Error deleting card:", error);
+            return false;
+        }
+    }
+
+    public async updateCardQuestion(
+        card: card,
+        question: string,
+    ): Promise<boolean> {
+        try {
+            await this.db.run(
+                "UPDATE cards SET question = :question WHERE cardId = :cardId AND deckId = :deckId",
+                {
+                    ":question": question,
+                    ":cardId": card.cardId,
+                    ":deckId": card.deckId,
+                },
+            );
+        } catch (error) {
+            console.error("Error updating card question:", error);
+            return false;
+        }
+    }
+
+    public async updateCardAnswer(
+        card: card,
+        answer: string,
+    ): Promise<boolean> {
+        try {
+            await this.db.run(
+                "UPDATE cards SET answer = :answer WHERE cardId = :cardId AND deckId = :deckId",
+                {
+                    ":answer": answer,
+                    ":cardId": card.cardId,
+                    ":deckId": card.deckId,
+                },
+            );
+        } catch (error) {
+            console.error("Error updating card answer:", error);
+            return false;
+        }
+    }
+
+    public async updateCardLaters(
+        card: card,
+        laters: string,
+    ): Promise<boolean> {
+        try {
+            await this.db.run(
+                "UPDATE cards SET laters = :laters WHERE cardId = :cardId AND deckId = :deckId",
+                {
+                    ":laters": laters,
+                    ":cardId": card.cardId,
+                    ":deckId": card.deckId,
+                },
+            );
+        } catch (error) {
+            console.error("Error updating card laters:", error);
+            return false;
+        }
+    }
+
+    public async updateCardDueDate(
+        card: card,
+        dueDate: string,
+    ): Promise<boolean> {
+        try {
+            await this.db.run(
+                "UPDATE cards SET dueDate = :dueDate WHERE cardId = :cardId AND deckId = :deckId",
+                {
+                    ":dueDate": dueDate,
+                    ":cardId": card.cardId,
+                    ":deckId": card.deckId,
+                },
+            );
+        } catch (error) {
+            console.error("Error updating card dueDate:", error);
             return false;
         }
     }
